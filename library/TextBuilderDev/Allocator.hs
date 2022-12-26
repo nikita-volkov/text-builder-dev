e1428e0b12036090554577a23fc536a90b07e519
Nikita Volkov <nikita.y.volkov@mail.ru>
2022-12-26 12:46:43 +0300

    Make inlining conditional

--- library/TextBuilderDev/Allocator.hs
+++ library/TextBuilderDev/Allocator.hs
@@ -144,23 +144,24 @@ unicodeCodePoint x =
 #endif
 
 -- | Single code-unit UTF-8 character.
-{-# INLINEABLE utf8CodeUnits1 #-}
 utf8CodeUnits1 :: Word8 -> Allocator
 #if MIN_VERSION_text(2,0,0)
+{-# INLINEABLE utf8CodeUnits1 #-}
 utf8CodeUnits1 unit1 = Allocator writer 1 
   where
     writer = ArrayWriter $ \array offset ->
       TextArray.unsafeWrite array offset unit1
         $> succ offset
 #else
+{-# INLINE utf8CodeUnits1 #-}
 utf8CodeUnits1 unit1 =
   Utf16View.utf8CodeUnits1 unit1 utf16CodeUnits1 utf16CodeUnits2
 #endif
 
 -- | Double code-unit UTF-8 character.
-{-# INLINEABLE utf8CodeUnits2 #-}
 utf8CodeUnits2 :: Word8 -> Word8 -> Allocator
 #if MIN_VERSION_text(2,0,0)
+{-# INLINEABLE utf8CodeUnits2 #-}
 utf8CodeUnits2 unit1 unit2 = Allocator writer 2 
   where
     writer = ArrayWriter $ \array offset -> do
@@ -168,14 +169,15 @@ utf8CodeUnits2 unit1 unit2 = Allocator writer 2
       TextArray.unsafeWrite array (offset + 1) unit2
       return $ offset + 2
 #else
+{-# INLINE utf8CodeUnits2 #-}
 utf8CodeUnits2 unit1 unit2 =
   Utf16View.utf8CodeUnits2 unit1 unit2 utf16CodeUnits1 utf16CodeUnits2
 #endif
 
 -- | Triple code-unit UTF-8 character.
-{-# INLINEABLE utf8CodeUnits3 #-}
 utf8CodeUnits3 :: Word8 -> Word8 -> Word8 -> Allocator
 #if MIN_VERSION_text(2,0,0)
+{-# INLINEABLE utf8CodeUnits3 #-}
 utf8CodeUnits3 unit1 unit2 unit3 = Allocator writer 3 
   where
     writer = ArrayWriter $ \array offset -> do
@@ -184,14 +186,15 @@ utf8CodeUnits3 unit1 unit2 unit3 = Allocator writer 3
       TextArray.unsafeWrite array (offset + 2) unit3
       return $ offset + 3
 #else
+{-# INLINE utf8CodeUnits3 #-}
 utf8CodeUnits3 unit1 unit2 unit3 =
   Utf16View.utf8CodeUnits3 unit1 unit2 unit3 utf16CodeUnits1 utf16CodeUnits2
 #endif
 
 -- | UTF-8 character out of 4 code units.
-{-# INLINEABLE utf8CodeUnits4 #-}
 utf8CodeUnits4 :: Word8 -> Word8 -> Word8 -> Word8 -> Allocator
 #if MIN_VERSION_text(2,0,0)
+{-# INLINEABLE utf8CodeUnits4 #-}
 utf8CodeUnits4 unit1 unit2 unit3 unit4 = Allocator writer 4 
   where
     writer = ArrayWriter $ \array offset -> do
@@ -201,16 +204,18 @@ utf8CodeUnits4 unit1 unit2 unit3 unit4 = Allocator writer 4
       TextArray.unsafeWrite array (offset + 3) unit4
       return $ offset + 4
 #else
+{-# INLINE utf8CodeUnits4 #-}
 utf8CodeUnits4 unit1 unit2 unit3 unit4 =
   Utf16View.utf8CodeUnits4 unit1 unit2 unit3 unit4 utf16CodeUnits1 utf16CodeUnits2
 #endif
 
 -- | Single code-unit UTF-16 character.
-{-# INLINEABLE utf16CodeUnits1 #-}
 utf16CodeUnits1 :: Word16 -> Allocator
 #if MIN_VERSION_text(2,0,0)
+{-# INLINE utf16CodeUnits1 #-}
 utf16CodeUnits1 = unicodeCodePoint . fromIntegral
 #else
+{-# INLINEABLE utf16CodeUnits1 #-}
 utf16CodeUnits1 unit =
   Allocator writer 1
   where
@@ -221,13 +226,14 @@ utf16CodeUnits1 unit =
 #endif
 
 -- | Double code-unit UTF-16 character.
-{-# INLINEABLE utf16CodeUnits2 #-}
 utf16CodeUnits2 :: Word16 -> Word16 -> Allocator
 #if MIN_VERSION_text(2,0,0)
+{-# INLINE utf16CodeUnits2 #-}
 utf16CodeUnits2 unit1 unit2 = unicodeCodePoint cp
   where
     cp = (((fromIntegral unit1 .&. 0x3FF) `shiftL` 10) .|. (fromIntegral unit2 .&. 0x3FF)) + 0x10000
 #else
+{-# INLINEABLE utf16CodeUnits2 #-}
 utf16CodeUnits2 unit1 unit2 =
   Allocator writer 2
   where
