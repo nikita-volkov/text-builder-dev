b6a38bd4e58dfb12d882a402c17fdde5d4db1f05
Nikita Volkov <nikita.y.volkov@mail.ru>
2025-04-03 08:35:14 +0300

    Redesign benchmarks

--- bench/Main.hs
+++ bench/Main.hs
@@ -1,6 +1,9 @@
 module Main where
 
 import Criterion.Main
+import Data.Function
+import Data.Text (Text)
+import qualified Data.Text.Encoding as D
 import qualified Data.Text.Lazy as C
 import qualified Data.Text.Lazy.Builder as B
 import qualified TextBuilderDev as A
@@ -8,71 +11,58 @@ import Prelude
 
 main :: IO ()
 main =
-  defaultMain
-    [ bgroup
-        "Left-biased mappend"
-        let {-# NOINLINE sampleBySize #-}
-            sampleBySize :: (Monoid a, IsString a) => Int -> (a -> Text) -> Text
-            sampleBySize size compile =
-              "фывапролдж"
-                & replicate size
-                & foldl' (<>) mempty
-                & compile
-         in [ bgroup
-                "1kB"
-                let sample = sampleBySize 100
-                 in [ bench "TextBuilderDev" (nf sample A.toText),
-                      bench "Data.Text.Lazy.Builder" (nf sample (C.toStrict . B.toLazyText))
-                    ],
-              bgroup
-                "1MB"
-                let sample = sampleBySize 100_000
-                 in [ bench "TextBuilderDev" (nf sample A.toText),
-                      bench "Data.Text.Lazy.Builder" (nf sample (C.toStrict . B.toLazyText))
-                    ]
-            ],
-      bgroup
-        "Right-biased mappend"
-        let {-# NOINLINE sampleBySize #-}
-            sampleBySize :: (Monoid a, IsString a) => Int -> (a -> Text) -> Text
-            sampleBySize size compile =
-              "фывапролдж"
-                & replicate size
-                & foldl' (flip (<>)) mempty
-                & compile
-         in [ bgroup
-                "1kB"
-                let sample = sampleBySize 100
-                 in [ bench "TextBuilderDev" (nf sample A.toText),
-                      bench "Data.Text.Lazy.Builder" (nf sample (C.toStrict . B.toLazyText))
-                    ],
-              bgroup
-                "1MB"
-                let sample = sampleBySize 100_000
-                 in [ bench "TextBuilderDev" (nf sample A.toText),
-                      bench "Data.Text.Lazy.Builder" (nf sample (C.toStrict . B.toLazyText))
-                    ]
-            ],
-      bgroup
-        "mconcat"
-        let {-# NOINLINE sampleBySize #-}
-            sampleBySize :: (Monoid a, IsString a) => Int -> (a -> Text) -> Text
-            sampleBySize size compile =
-              "фывапролдж"
-                & replicate size
-                & mconcat
-                & compile
-         in [ bgroup
-                "1kB"
-                let sample = sampleBySize 100
-                 in [ bench "TextBuilderDev" (nf sample A.toText),
-                      bench "Data.Text.Lazy.Builder" (nf sample (C.toStrict . B.toLazyText))
-                    ],
-              bgroup
-                "1MB"
-                let sample = sampleBySize 100_000
-                 in [ bench "TextBuilderDev" (nf sample A.toText),
-                      bench "Data.Text.Lazy.Builder" (nf sample (C.toStrict . B.toLazyText))
-                    ]
-            ]
-    ]
+  defaultMain byAlgorithm
+
+byAlgorithm :: [Benchmark]
+byAlgorithm =
+  [ bgroup "Left-biased mappend" $
+      bySize (foldl' (<>) mempty),
+    bgroup "Right-biased mappend" $
+      bySize (foldl' (flip (<>)) mempty),
+    bgroup "mconcat" $
+      bySize mconcat
+  ]
+
+bySize :: (forall a. (Monoid a) => [a] -> a) -> [Benchmark]
+bySize concat =
+  [ bgroup "100B" . bySubject concat $
+      replicate 10 "фывапролдж",
+    bgroup "1kB" . bySubject concat $
+      replicate 100 "фывапролдж",
+    bgroup "1MB" . bySubject concat $
+      replicate 100_000 "фывапролдж"
+  ]
+
+bySubject :: (forall a. (Monoid a) => [a] -> a) -> [Text] -> [Benchmark]
+bySubject concat texts =
+  [ bench
+      "TextBuilderDev.TextBuilder"
+      ( whnf
+          (A.toText . concat)
+          (fmap A.text texts)
+      ),
+    bench
+      "Data.Text.Encoding.StrictTextBuilder"
+      ( whnf
+          (D.strictBuilderToText . concat)
+          (fmap D.textToStrictBuilder texts)
+      ),
+    bench
+      "Data.Text.Lazy.Builder.Builder"
+      ( whnf
+          (C.toStrict . B.toLazyText . concat)
+          (fmap B.fromText texts)
+      ),
+    bench
+      "Data.Text.Text"
+      ( whnf
+          concat
+          texts
+      ),
+    bench
+      "Data.Text.Lazy.Text"
+      ( whnf
+          (C.toStrict . concat)
+          (fmap C.fromStrict texts)
+      )
+  ]
