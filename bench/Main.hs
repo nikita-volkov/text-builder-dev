cfd5b61992259ec3a27a48649d97a49ffe18306a
Nikita Volkov <nikita.y.volkov@mail.ru>
2025-04-04 15:47:13 +0300

    Polish benchmarks

--- bench/Main.hs
+++ bench/Main.hs
@@ -1,7 +1,6 @@
 module Main where
 
 import Data.Function
-import Data.Text (Text)
 import qualified Data.Text.Encoding as D
 import qualified Data.Text.Lazy as C
 import qualified Data.Text.Lazy.Builder as B
@@ -11,60 +10,52 @@ import Prelude
 
 main :: IO ()
 main =
-  defaultMain byAlgorithm
-
-byAlgorithm :: [Benchmark]
-byAlgorithm =
-  [ bgroup "Left-biased mappend" $
-      bySize (foldl' (<>) mempty),
-    bgroup "Right-biased mappend" $
-      bySize (foldl' (flip (<>)) mempty),
-    bgroup "mconcat" $
-      bySize mconcat
-  ]
-
-bySize :: (forall a. (Monoid a) => [a] -> a) -> [Benchmark]
-bySize concat =
-  [ bgroup "100B" . bySubject concat $
-      replicate 10 "фывапролдж",
-    bgroup "1kB" . bySubject concat $
-      replicate 100 "фывапролдж",
-    bgroup "10kB" . bySubject concat $
-      replicate 1_000 "фывапролдж",
-    bgroup "100kB" . bySubject concat $
-      replicate 10_000 "фывапролдж"
-  ]
-
-bySubject :: (forall a. (Monoid a) => [a] -> a) -> [Text] -> [Benchmark]
-bySubject concat texts =
-  [ bench
-      "TextBuilderDev.TextBuilder"
-      ( whnf
-          (A.toText . concat)
-          (fmap A.text texts)
-      ),
-    bench
-      "Data.Text.Encoding.StrictTextBuilder"
-      ( whnf
-          (D.strictBuilderToText . concat)
-          (fmap D.textToStrictBuilder texts)
-      ),
-    bench
-      "Data.Text.Lazy.Builder.Builder"
-      ( whnf
-          (C.toStrict . B.toLazyText . concat)
-          (fmap B.fromText texts)
-      ),
-    bench
-      "Data.Text.Text"
-      ( whnf
-          concat
-          texts
-      ),
-    bench
-      "Data.Text.Lazy.Text"
-      ( whnf
-          (C.toStrict . concat)
-          (fmap C.fromStrict texts)
-      )
-  ]
+  defaultMain
+    [ bgroup "Text concatenation" textConcatenation
+    ]
+  where
+    textConcatenation =
+      [ bgroup "Left-biased mappend" $ byConcat $ foldl' (<>) mempty,
+        bgroup "Right-biased mappend" $ byConcat $ foldl' (flip (<>)) mempty,
+        bgroup "mconcat" $ byConcat $ mconcat
+      ]
+      where
+        byConcat (concat :: forall a. (Monoid a) => [a] -> a) =
+          [ bgroup "100B" $ byTexts $ replicate 10 "фывапролдж",
+            bgroup "1kB" $ byTexts $ replicate 100 "фывапролдж",
+            bgroup "10kB" $ byTexts $ replicate 1_000 "фывапролдж",
+            bgroup "100kB" $ byTexts $ replicate 10_000 "фывапролдж"
+          ]
+          where
+            byTexts texts =
+              [ bench
+                  "TextBuilderDev.TextBuilder"
+                  ( whnf
+                      (A.toText . concat)
+                      (fmap A.text texts)
+                  ),
+                bench
+                  "Data.Text.Encoding.StrictTextBuilder"
+                  ( whnf
+                      (D.strictBuilderToText . concat)
+                      (fmap D.textToStrictBuilder texts)
+                  ),
+                bench
+                  "Data.Text.Lazy.Builder.Builder"
+                  ( whnf
+                      (C.toStrict . B.toLazyText . concat)
+                      (fmap B.fromText texts)
+                  ),
+                bench
+                  "Data.Text.Text"
+                  ( whnf
+                      concat
+                      texts
+                  ),
+                bench
+                  "Data.Text.Lazy.Text"
+                  ( whnf
+                      (C.toStrict . concat)
+                      (fmap C.fromStrict texts)
+                  )
+              ]
